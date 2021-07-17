2b4cd6773ed7b38139d585921e475030fa086732
Suresha Hp <47083959+hpsuresh12345@users.noreply.github.com>
2021-07-17 23:57:28 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -70,3 +70,57 @@ with dataset:
     plt.xticks(index, label, fontsize=11, rotation=90, fontweight="bold")
     plt.title('Top 25 Words after preprocessing', fontsize=12, fontweight="bold")
     st.pyplot()
+
+    with vader_sentiment:
+        st.header('Vader Sentiment ')
+        html_temp = """
+                    <div style="background-color:#025122 ;padding:10px">
+                    <h2 style="color:white;text-align:center;">
+                    Vader sentiment analysis - Python </h2>
+                    </div>
+                    """
+        st.markdown(html_temp, unsafe_allow_html=True)
+        st.header('Vader sentiment analysis for the tweets collected')
+        # Create an object of Vader Sentiment Analyzer
+        df1 = pd.read_csv('https://raw.githubusercontent.com/hpsuresh12345/Streamlit/main/data/Vader.csv')
+        vader_analyzer = SentimentIntensityAnalyzer()
+
+        # Draw Plot
+        fig, ax = plt.subplots(figsize=(10, 6), subplot_kw=dict(aspect="equal"), dpi=80)
+
+        data = [df1.sentiment.value_counts()[-1], df1.sentiment.value_counts()[0], df1.sentiment.value_counts()[1]]
+        categories = ['Negative', 'Neutral', 'Positive']
+        explode = [0.05, 0.05, 0.05]
+
+
+        def func(pct, allvals):
+            absolute = int(pct / 100. * np.sum(allvals))
+            return "{:.1f}% ({:d} )".format(pct, absolute)
+
+
+        wedges, texts, autotexts = ax.pie(data,
+                                          autopct=lambda pct: func(pct, data),
+                                          textprops=dict(color="w"),
+                                          colors=['#e55039', '#3c6382', '#78e08f'],
+                                          startangle=140,
+                                          explode=explode)
+
+        # Decoration
+        ax.legend(wedges, categories, title="Sentiment", loc="center left", bbox_to_anchor=(1, 0.2, 0.5, 1))
+        plt.setp(autotexts, size=10, weight=700)
+        ax.set_title("Number of Tweets by Sentiment", fontsize=12, fontweight="bold")
+        st.pyplot()
+
+        # histogram
+        labels = ['Negative', 'Neutral', 'Positive']
+        freq = [df1.sentiment.value_counts()[-1], df1.sentiment.value_counts()[0], df1.sentiment.value_counts()[1]]
+        index = np.arange(len(freq))
+
+        plt.figure(figsize=(8, 6))
+        plt.bar(index, freq, alpha=0.8, color='tomato')
+        plt.xlabel('Sentiment', fontsize=13)
+        plt.ylabel('Number of Tweets', fontsize=13)
+        plt.xticks(index, labels, fontsize=11, fontweight="bold")
+        plt.title('Number of Tweets per Sentiment', fontsize=12, fontweight="bold")
+        plt.ylim(0, len(df1['Tweet Text']))
+        st.pyplot()
